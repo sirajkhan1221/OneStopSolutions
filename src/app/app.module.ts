8b9b6ddb82c0a94809724cc5fd4e0b07dba626a2
siraj1221 <siraj1221@gmail.com>
2018-10-09 15:21:27 -0400

    Layout Page on index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import"C:/Users/MiyaBhai_786/Desktop/WorkSpace/OneStop/node_modules/@angular/material/prebuilt-themes/indigo-pink.css"
 
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+     
   ],
   imports: [
     BrowserModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
